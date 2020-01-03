9c96ba3b87c92f97d1d0f5f99c11eac8b4069fb3
Simão Gonçalves <simaog10@gmail.com>
2020-01-03 17:03:02 +0000

    fix

--- Controllers/ProfessoresController.cs
+++ Controllers/ProfessoresController.cs
@@ -85,6 +85,7 @@ namespace HealthUp.Controllers
                 Text = "CC: " + s.NumCC + " | " + s.NumSocioNavigation.Nome,
                 Value = s.NumCC
             });
+
             return View(socio);
         }
     }
