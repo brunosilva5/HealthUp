05976300e838c058cb45a67e159f8b7395d482b1
Simão Gonçalves <simaog10@gmail.com>
2020-01-07 15:40:18 +0000

    DA fix (SolicitacaoProfessor)

--- Models/SolicitacaoProfessor.cs
+++ Models/SolicitacaoProfessor.cs
@@ -16,16 +16,23 @@ namespace HealthUp.Models
         }
 
         [Key]
+        [Display(Name = "Id de solicitação")]
         public int? IdSolicitacao { get; set; }
+
+        [Display(Name = "Número de administrador")]
         public string NumAdmin { get; set; }
+        
         [DataType(DataType.Date)]
         public DateTime? Data { get; set; }
 
         [ForeignKey(nameof(NumAdmin))]
         [InverseProperty(nameof(Admin.SolicitacaoProfessor))]
+        [Display(Name = "Número de navegação do administrador")]
         public virtual Admin NumAdminNavigation { get; set; }
+        
         [InverseProperty("IdSolicitacaoNavigation")]
         public virtual ICollection<Professor> Professor { get; set; }
+        
         [InverseProperty("IdSolicitacaoNavigation")]
         public virtual ICollection<Socio> Socio { get; set; }
     }
