49c12206cd32a91be13aa018f8c94643fe9b522f
Simão Gonçalves <simaog10@gmail.com>
2020-01-07 15:30:04 +0000

    DA fix (Mensagem)

--- Models/Mensagem.cs
+++ Models/Mensagem.cs
@@ -10,17 +10,26 @@ namespace HealthUp.Models
     public partial class Mensagem
     {
         [Key]
+        [Display(Name = "Id da mensagem")]
         public int IdMensagem { get; set; }
+
+        [Display(Name = "Id da pessoa")]
         public string IdPessoa { get; set; }
+        
         public DateTime? DataEnvio { get; set; }
+        
         public bool Lida { get; set; }
+        
         public bool Arquivada { get; set; }
+        
         [Required(ErrorMessage = "Este campo é de preenchimento obrigatório!")]
         [StringLength(500)]
+        [Display(Name = "Conteúdo")]
         public string Conteudo { get; set; }
 
         [ForeignKey(nameof(IdPessoa))]
         [InverseProperty(nameof(Pessoa.Mensagem))]
+        [Display(Name = "Id de navegação")]
         public virtual Pessoa IdNavigation { get; set; }
     }
 }
