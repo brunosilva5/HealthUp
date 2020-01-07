88bf9ea2228168f430f015aa62499d6e7ed21fd4
Simão Gonçalves <simaog10@gmail.com>
2020-01-07 15:21:40 +0000

    DA fix (AulaGrupo)

--- Models/AulaGrupo.cs
+++ Models/AulaGrupo.cs
@@ -12,22 +12,27 @@ namespace HealthUp.Models
     {
         [Key]
         public int IdAula { get; set; }
+        
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [StringLength(30)]
         [Display(Name = "Aula de Grupo")]
         public string Nome { get; set; }
+        
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [StringLength(100)]
         [Remote("IsValidFotografiaDivulgacao", "Validation_Files", HttpMethod = "POST", ErrorMessage = "A fotografia tem de ser no formato .jpg")]
         [Display(Name = "Fotografia")]
         public string FotografiaDivulgacao { get; set; }
+        
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [StringLength(100)]
         [Remote("IsValidVideoDivulgacao", "Validation_Files", HttpMethod = "POST", ErrorMessage = "O video tem de ser no formato .mp4")]
         [Display(Name = "Video")]
         public string VideoDivulgacao { get; set; }
+        
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [StringLength(500)]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
         // 
