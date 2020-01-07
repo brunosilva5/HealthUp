328149336883b1c61aa631c1c734b64c368d5cd9
Simão Gonçalves <simaog10@gmail.com>
2020-01-07 15:23:07 +0000

    DA fix (Contem)

--- Models/Contem.cs
+++ Models/Contem.cs
@@ -14,21 +14,27 @@ namespace HealthUp.Models
         [Key]
         public int IdPlano { get; set; }
         public int IdExercicio { get; set; }
+        
         [Display(Name ="Número de repetições")]
         [Range(0, Int32.MaxValue)]
         public int NumRepeticoes { get; set; }
+        
         [Display(Name = "Período de descanso")]
         [Range(0, Int32.MaxValue)]
         public int PeriodoDescanso { get; set; }
+        
         [Display(Name = "Quantidade de séries")]
         [Range(0,Int32.MaxValue)]
         public int QuantidadeSeries { get; set; }
 
         [ForeignKey(nameof(IdExercicio))]
         [InverseProperty(nameof(Exercicio.Contem))]
+        [Display(Name = "Id de navegação do exercício")]
         public virtual Exercicio IdExercicioNavigation { get; set; }
+        
         [ForeignKey(nameof(IdPlano))]
         [InverseProperty(nameof(PlanoTreino.Contem))]
+        [Display(Name = "Id de navegação do plano de treino")]
         public virtual PlanoTreino IdPlanoNavigation { get; set; }
 
 
